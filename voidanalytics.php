60b517ea7a85d94750370b2e52d88d017980a708
VoidAnalytics <156999921+VoidAnalytics@users.noreply.github.com>
2024-07-29 17:41:48 +0300

    Create voidanalytics.php

---
+++ voidanalytics.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: Void Analytics Integration
+Description: A simple plugin to add Void Analytics tracking script to each page.
+Version: 1.4.0
+Author: Void Analytics
+*/
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
+// Enqueue the Void Analytics script in the head
+function add_void_analytics_script() {
+    echo '<script src="https://cdn.voidanalytics.com/latest.min.js"></script>';
+}
+add_action('wp_head', 'add_void_analytics_script');
+
+// Add noscript tag at the end of the body
+function add_void_analytics_noscript() {
+    echo '<noscript><img src="https://cdn.voidanalytics.com/drop.gif" alt=""/></noscript>';
+}
+add_action('wp_footer', 'add_void_analytics_noscript');
